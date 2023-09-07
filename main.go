f4e6ebc385876f14c0936f649708b16b15e02c52
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2023-09-07 15:53:12 +0800

    proper serve static files

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+
+		p := filepath.Join(".", filepath.Clean(r.URL.Path))
+
+		if f, err := os.Stat(p); errors.Is(err, os.ErrNotExist) || f.IsDir() {
+			http.ServeFile(w, r, "index.html")
+		} else {
+			http.ServeFile(w, r, p)
+		}
 	})
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
